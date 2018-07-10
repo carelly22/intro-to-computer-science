1f1272be90ab69672ea758e05d927708a10c9402
Carell Ayanna Hamil <ayannahamil@gmail.com>
2018-07-10 16:14:08 -0400

    Add quiz 11.9

---
+++ stooges2.py
@@ -0,0 +1,20 @@
+# We defined:
+
+stooges = ['Moe','Larry','Curly']
+
+# but in some Stooges films, Curly was
+# replaced by Shemp.
+
+# Write one line of code that changes
+# the value of stooges to be:
+
+#['Moe','Larry','Shemp']
+
+# but does not create a new List
+# object.
+
+#assign the third position in stooges to Shemp
+stooges[2] = "Shemp"
+
+#print out the result
+print(stooges)
